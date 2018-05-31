226e5c3ca93ca1d5ae08a4798f0618fb61b9b425
mohamed_nabil <m.nabil.fci2015@gmail.com>
2018-05-31 15:09:24 +0300

    added INSPINIA basic views for admin

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,23 @@
+<div class="footer">
+        <div class="pull-right">
+            10GB of <strong>250GB</strong> Free.
+        </div>
+        <div>
+            <strong>Copyright</strong> Example Company &copy; 2014-2017
+        </div>
+    </div>
+
+</div>
+</div>
+<!-- Mainly scripts -->
+<script src="{{URL::asset('js/jquery-3.1.1.min.js')}}" type="text/javascript"></script>
+<script src="{{URL::asset('js/bootstrap.min.js')}}" type="text/javascript"></script>
+<script src="{{URL::asset('js/plugins/metisMenu/jquery.metisMenu.js')}}" type="text/javascript"></script>
+<script src="{{URL::asset('js/plugins/slimscroll/jquery.slimscroll.min.js')}}" type="text/javascript"></script>
+<script src="{{URL::asset('js/inspinia.js')}}" type="text/javascript"></script>
+<script src="{{URL::asset('js/plugins/pace/pace.min.js')}}" type="text/javascript"></script>
+
+
+</body>
+
+</html>
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>INSPINIA | Empty Page</title>
+    <link href="{{URL::asset('css/bootstrap.min.css')}}" rel="stylesheet" type="text/css" />
+    <link href="{{URL::asset('font-awesome/css/font-awesome.css')}}" rel="stylesheet" type="text/css" />
+    <link href="{{URL::asset('css/animate.css')}}" rel="stylesheet" type="text/css" />
+    <link href="{{URL::asset('css/style.css')}}" rel="stylesheet" type="text/css" />
+       
+
+</head>
+
+<body class="">
+    <div id="wrapper">
+
+        @include ('layouts.side_nav')
+
+
+        <div id="page-wrapper" class="gray-bg">
+        <div class="row border-bottom">
+        <nav class="navbar navbar-static-top  " role="navigation" style="margin-bottom: 0">
+        <div class="navbar-header">
+            <a class="navbar-minimalize minimalize-styl-2 btn btn-primary " href="#"><i class="fa fa-bars"></i> </a>
+        </div>
+            <ul class="nav navbar-top-links navbar-right">
+                <li>
+                    <span class="m-r-sm text-muted welcome-message">Welcome to INSPINIA+ Admin Theme.</span>
+                </li>
+
+                <li>
+                    <a href="login.html">
+                        <i class="fa fa-sign-out"></i> Log out
+                    </a>
+                </li>
+            </ul>
+
+        </nav>
+    </div>
+
+
+        
+
